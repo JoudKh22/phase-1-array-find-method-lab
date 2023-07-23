280bbd4a4f3e4e1213bae6f8fd361a6d7a9adfe4
Joud Khanji <joud.hanji@gmail.com>
2023-07-23 15:43:28 +0300

    test

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// code your solution here
+function superbowlWin(record) {
+    const winningRecord = record.find((game) => game.result === "W");
+  
+    if (winningRecord) {
+      return winningRecord.year;
+    } else {
+      return undefined;
+    }
+  }
+  
+  
+ 
+ 
